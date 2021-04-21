0aac47058cb43921163985da16b33acedf0b3383
Terry <terrykuo501@gmail.com>
2021-04-21 17:47:45 +0800

    Initial Commit

---
+++ Memefy/MemefyApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemefyApp.swift
+//  Memefy
+//
+//  Created by Terry Kuo on 2021/4/21.
+//
+
+import SwiftUI
+
+@main
+struct MemefyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
