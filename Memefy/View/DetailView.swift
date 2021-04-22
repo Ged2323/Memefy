6ad50843348d258b8cf1700de10092ecf75c919c
Terry <terrykuo501@gmail.com>
2021-04-22 13:41:02 +0800

    draggable text and take screenshot

--- Memefy/View/DetailView.swift
+++ Memefy/View/DetailView.swift
@@ -14,43 +14,73 @@ struct DetailView: View {
     var imageTitle: String = "Drake Meme"
     var boxCountNumber = 2
     @State var addedlabel: [String] = ["", "" , "", "" , "", ""]
+    //@State private var navBarHeight: CGFloat = 0.0
+    var wholeScreenWidth = UIScreen.main.bounds.width
+    var memeImageHeight = UIScreen.main.bounds.height / 2.5
+    //var saveAreaHeight = UIApplication.shared.windows[0].safeAreaInsets.top
+    @Environment(\.presentationMode) private var presentationMode
+    @State var minY: CGFloat = 0.0
+    @State var minX: CGFloat = 0.0
     
     var body: some View {
         NavigationView {
             
             VStack {
                 
-                KFImage(URL(string: imageURL))
-                    .resizable()
-                    .aspectRatio(contentMode: .fit)
                 
-                
+                memeImageView
                 
                 Spacer()
                 
                 ForEach(0..<boxCountNumber) { index in
-                    ZStack{
-                        VStack {
-                            TextField("Label \(index + 1)", text: $addedlabel[index],onCommit: {
-                                print(addedlabel)
-                            })
-                            .textFieldStyle(RoundedBorderTextFieldStyle())
-                        }
-                        showTextOrNot(label: addedlabel[index])
-                            .onTapGesture {
-                                print(addedlabel[index])
-                            }
-                    }
+                    TextField("Label \(index + 1)", text: $addedlabel[index])
+                    .padding(.horizontal)
+                    .textFieldStyle(RoundedBorderTextFieldStyle())
                 }
                 
                 Button(action: {
+                    if let image = memeImageView.takeScreenshot(origin: CGPoint(x: 0, y: 151.0), size: CGSize(width: wholeScreenWidth, height: memeImageHeight)) {
+                        UIImageWriteToSavedPhotosAlbum(image, nil, nil, nil)
+                        presentationMode.wrappedValue.dismiss()
+                    }
                 }, label: {
                     Text("Save Image")
                 })
             }
             .padding()
             .navigationBarTitle(imageTitle,displayMode: .inline)
+//            .background(NavBarAccessor{ navBar in
+//                self.navBarHeight = navBar.bounds.height
+//            })
         }
+    }
+    
+    var memeImageView: some View {
+        ZStack {
+            GeometryReader { geo in
+                mainImage
+                    .onTapGesture {
+                        self.minY = geo.frame(in: .global).minY
+                        self.minX = geo.frame(in: .global).minX
+                        print("minX is \(self.minX)")
+                        print("minY is \(self.minY)")
+                    }
+            }
+            ForEach(addedlabel, id:\.self) { label in
+                DraggableLabel(text: label)
+            }
+        }
+        .clipped() //hide the text when drag over the boarder of image
+    }
+    
+    var mainImage: some View {
+        KFImage(URL(string: imageURL))
+            .placeholder {
+                ProgressView()
+            }
+            .resizable()
+            .aspectRatio(contentMode: .fit)
+            .frame(height: memeImageHeight)
     }
 }
 
@@ -60,26 +90,26 @@ struct DetailView: View {
 //    }
 //}
 
-extension DetailView {
-    @ViewBuilder
-    func showTextOrNot(label: String) -> some View {
-        if label.isEmpty {
-            Text("")
-        } else {
-            Text("\(label)")
-                .font(.title2)
-                .foregroundColor(Color.black)
-                .padding()
-        }
-    }
-}
-
-struct DetailViewContainer<Content: View>: View {
-    let content: Content
-    init(@ViewBuilder content: () -> Content) {
-        self.content = content()
-    }
-    var body: some View {
-        content
-    }
-}
+//extension DetailView {
+//    @ViewBuilder
+//    func showTextOrNot(label: String) -> some View {
+//        if label.isEmpty {
+//            Text("")
+//        } else {
+//            Text("\(label)")
+//                .font(.title2)
+//                .foregroundColor(Color.black)
+//                .padding()
+//        }
+//    }
+//}
+//
+//struct DetailViewContainer<Content: View>: View {
+//    let content: Content
+//    init(@ViewBuilder content: () -> Content) {
+//        self.content = content()
+//    }
+//    var body: some View {
+//        content
+//    }
+//}
